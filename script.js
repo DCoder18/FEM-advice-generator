c90afabab482b1aba7d69e462f7836a3072f0e66
Devika <devikasujith12@gmail.com>
2022-05-16 01:39:30 +0400

    Script WIP

---
+++ script.js
@@ -0,0 +1,24 @@
+const API_URL = 'https://api.adviceslip.com/advice';
+
+const elHeading = document.querySelector('.advice__id');
+const elText = document.querySelector('.advice__text');
+const elBtn = document.querySelector('.btn');
+
+let id = '';
+let advice = '';
+
+async function getRandomQuote() {
+    const response = await fetch(API_URL);
+    const data = await response.json();
+    const {id, advice} = data.slip;
+    console.log(id, advice);
+}
+
+elBtn.addEventListener('click', () => {
+    getRandomQuote();
+
+    elHeading.textContent = id;
+    elText.textContent = advice;
+})
+
+// getRandomQuote();
