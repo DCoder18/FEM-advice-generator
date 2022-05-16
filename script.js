c848769d4e863a4e2c6ce8eedada5403d8cfe37c
Devika <devikasujith12@gmail.com>
2022-05-16 13:24:04 +0400

    Project completed

--- script.js
+++ script.js
@@ -4,21 +4,14 @@ const elHeading = document.querySelector('.advice__id');
 const elText = document.querySelector('.advice__text');
 const elBtn = document.querySelector('.btn');
 
-let id = '';
-let advice = '';
-
 async function getRandomQuote() {
     const response = await fetch(API_URL);
     const data = await response.json();
     const {id, advice} = data.slip;
-    console.log(id, advice);
+    
+    elHeading.textContent = `Advice #${id}`;
+    elText.textContent = `"${advice}"`;
 }
 
-elBtn.addEventListener('click', () => {
-    getRandomQuote();
-
-    elHeading.textContent = id;
-    elText.textContent = advice;
-})
-
-// getRandomQuote();
+getRandomQuote();
+elBtn.addEventListener('click', getRandomQuote);
